6877c23f69f5cf67fd6072baa7c64d56b719f899
imvarungupta <71177150+imvarungupta@users.noreply.github.com>
2020-09-12 23:02:20 +0530

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -2,35 +2,32 @@ import optparse
 import socket
 from socket import * 
 def connScan(tgtHost, tgtPort):
-	try:
-		connSkt = socket(AF_INET, SOCK_STREAM)
-		connSkt.connect((tgtHost, tgtPort))
-		connSkt.send('ViolentPython\r\n')
-		results = connSkt.recv(100)
-		print '[+] %d tcp open' % tgtPort
-		print '[+]' + str(results)
-		connSkt.close()
-	except:
-		print '[-]%d tcp closed' % tgtPort
+   try:
+	connSkt = socket(AF_INET, SOCK_STREAM)
+	connSkt.connect((tgtHost, tgtPort))
+	connSkt.send('ViolentPython\r\n')
+	results = connSkt.recv(100)
+	print '[+] %d tcp open' % tgtPort
+	print '[+]' + str(results)
+	connSkt.close()
+  except:
+	print '[-]%d tcp closed' % tgtPort
 	
-
 def portScan(tgtHost, tgtPorts):
-	try:
-		tgtIP = gethostbyname(tgtHost)
-	except:
-		print "[-] Cannot resolve '%s' : Unknown host " %tgtHost
-		return
-	try:
-		tgtName = gethostbyip(tgtIP)
-		print '\n[+] Scan results for' + tgtName[0]
-	except:
-		print '\n[+] Scan results for: ' + tgtIP
-	setdefaulttimeout(1)
-	for tgtPort in tgtPorts:
-		print 'Scanning Port' + tgtPort
-		connScan(tgtHost, int(tgtPort))
-
-
+   try:
+	tgtIP = gethostbyname(tgtHost)
+   except:
+	print "[-] Cannot resolve '%s' : Unknown host " %tgtHost
+	return
+   try:
+        tgtName = gethostbyip(tgtIP)
+	print '\n[+] Scan results for' + tgtName[0]
+   except:
+	print '\n[+] Scan results for: ' + tgtIP
+   setdefaulttimeout(1)
+   for tgtPort in tgtPorts:
+        print 'Scanning Port' + tgtPort
+	connScan(tgtHost, int(tgtPort))
 def main():
 	parser = optparse.OptionParser("usage%prog "+ "-H <target host> -p <target port>")
 	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
@@ -39,10 +36,8 @@ def main():
 	tgtHost = options.tgtHost
 	tgtPorts = str(options.tgtPort).split(',') 
 	if (tgtHost == None) | (tgtPorts[0] == None):
-		print '[-] You must specify a target host and port[s].'
-		exit(0)
+	   print '[-] You must specify a target host and port[s].'
+	exit(0)
 	portScan(tgtHost, tgtPorts)
 	
-
-
 main()
