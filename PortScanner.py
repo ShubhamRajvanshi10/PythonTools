1cb8cd6b45404fa44cb5865a3c41a458470b3181
imvarungupta <71177150+imvarungupta@users.noreply.github.com>
2020-09-13 14:11:50 +0530

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -2,8 +2,8 @@ import optparse
 import socket
 from socket import * 
 def connScan(tgtHost, tgtPort):
-   try:
-	connSkt = socket(AF_INET, SOCK_STREAM)
+    try:
+        connSkt = socket(AF_INET, SOCK_STREAM)
 	connSkt.connect((tgtHost, tgtPort))
 	connSkt.send('ViolentPython\r\n')
 	results = connSkt.recv(100)
@@ -14,8 +14,8 @@ def connScan(tgtHost, tgtPort):
 	print '[-]%d tcp closed' % tgtPort
 	
 def portScan(tgtHost, tgtPorts):
-   try:
-	tgtIP = gethostbyname(tgtHost)
+    try:
+        tgtIP = gethostbyname(tgtHost)
    except:
 	print "[-] Cannot resolve '%s' : Unknown host " %tgtHost
 	return
@@ -29,15 +29,15 @@ def portScan(tgtHost, tgtPorts):
         print 'Scanning Port' + tgtPort
 	connScan(tgtHost, int(tgtPort))
 def main():
-	parser = optparse.OptionParser("usage%prog "+ "-H <target host> -p <target port>")
-	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-	parser.add_option('-p', dest='tgtPort', type='string', help='specify target port[s] separated by comma')
-	(options, args) = parser.parse_args()
-	tgtHost = options.tgtHost
-	tgtPorts = str(options.tgtPort).split(',') 
-	if (tgtHost == None) | (tgtPorts[0] == None):
-	   print '[-] You must specify a target host and port[s].'
-	exit(0)
-	portScan(tgtHost, tgtPorts)
+    parser = optparse.OptionParser("usage%prog "+ "-H <target host> -p <target port>")
+    parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+    parser.add_option('-p', dest='tgtPort', type='string', help='specify target port[s] separated by comma')
+    (options, args) = parser.parse_args()
+    tgtHost = options.tgtHost
+    tgtPorts = str(options.tgtPort).split(',') 
+    if (tgtHost == None) | (tgtPorts[0] == None):
+        print '[-] You must specify a target host and port[s].'
+    exit(0)
+        portScan(tgtHost, tgtPorts)
 	
 main()
