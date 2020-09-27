6bd679edc7c9dd565726b042470a2c4511091ad5
tarunrajvanshi <54480621+tarunrajvanshi@users.noreply.github.com>
2020-09-27 14:44:01 +0530

    Changes made. 1
    Changes made.

--- MoveNextGen.py
+++ MoveNextGen.py
@@ -2,5 +2,5 @@ import os
 with open ('uniqDll.txt', 'r') as Dl:
     for line in Dl:
         rec = line.strip()
-	NameWithoutDll , extra = rec.split('.dll')
-	print rec + "|" + NameWithoutDll + ";<.*>d__.*|MoveNext|0|0"
+	    NameWithoutDll , extra = rec.split('.dll')
+	    print rec + "|" + NameWithoutDll + ";<.*>d__.*|MoveNext|0|0"
